af43e5c1c3e0d237cc0331ac8269cabcfb398591
mrfosip <hamedfallaha@berkeley.edu>
2015-04-23 20:28:31 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cleverbot2::Application.config.secret_key_base = '33f49a80b7cdb45e5b5d1405e9632f33a36c3ec781ec044cb9d435a3e0ea15637f5e7497385c5ac56f3527d4bc3a473ecc624558e6f687333e8bbd8dd61fc3fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cleverbot2::Application.config.session_store :cookie_store, key: '_cleverbot2_session'
